0d6c86c82aeb57a8211cec9a5324521cea692746
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2019-11-16 21:14:04 -0300

    Created the loader and fixed the migration file

--- src/database/migrations/20191116225617-create-users.js
+++ src/database/migrations/20191116225617-create-users.js
@@ -22,7 +22,7 @@ module.exports = {
       },
       provider: {
         type: Sequelize.BOOLEAN,
-        deafulValue: false,
+        defaultValue: false,
         allowNull: false,
       },
       created_at: {
