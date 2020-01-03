9d9d022bd631786fa7ff6c0413c9036caf6f0caf
Flavio Teixeira Lopes <flaviotxa@gmail.com>
2020-01-02 21:13:02 -0300

    Implemented cancelation mail feature

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '3697dc67806ec4',
+    pass: 'bddac4f798a8af',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com',
+  },
+};
